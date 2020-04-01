69e13ae3578db20bc4b20376a0dee7c2ea7c1690
igormoreira-computer.eng <igor_moreira@live.com>
2020-04-01 09:57:25 -0300

    .

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {Promi
 
 const port = process.env.PORT ||3000
 
+app.set('views', path.join(__dirname('views')))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
